c465033822b01ac34e4bd8cdaaea454a52394220
AntonErikssonCode <antonerikssonoboy@gmail.com>
2023-05-03 00:02:15 +0200

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  class Character {
+    name: string;
+    items: number[];
+  
+    constructor(name: string, items: number[]) {
+      this.name = name;
+      this.items = items;
+    }
+  }
+  
+  const mrAron = new Character("Mr (Sir) Aron", [1, 2, 3, 4, 5]);
+  const firoraLanka = new Character("Fiora Lanka", [1, 2, 3, 4, 5]);
+  const kenneth = new Character("Kenneth", [1, 2, 3, 4, 5]);
+
+  const allChars: Character[] = [mrAron, firoraLanka, kenneth];
+  const itemComponents = allChars.map((character, index) => (
+    <li key={character.name+index}>{character.name}</li>
+  ));
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <ul>{itemComponents}</ul>
     </div>
   );
 }
