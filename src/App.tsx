af192818bf173b04935b681f6516e827060f468e
AntonErikssonCode <antonerikssonoboy@gmail.com>
2023-05-06 17:15:11 +0200

    firebase setup

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,32 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import Card from "./components/card";
 
+
+
+import firebase from "firebase/app"
+import "firebase/firestore"
+import "firebase/auth"
+import {useAuthState} from "react-firebase-hooks/auth";
+import {useColletionData} from "react-firebase-hooks/firestore";
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+
+
+
+})
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+
+const apiKey = process.env.REACT_APP_TEST;
+console.log(apiKey);
 const theme = createTheme({
   palette: {
     mode: "dark",
