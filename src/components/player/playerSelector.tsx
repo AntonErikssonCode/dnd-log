b13a3631c875e78d44c1d57d239431c7067c88b0
AntonErikssonCode <antonerikssonoboy@gmail.com>
2023-05-14 18:16:53 +0200

    Fixed so data saves correct when input is changed

---
+++ src/components/player/playerSelector.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  Container,
+  Typography,
+  List,
+  ListItem,
+  Box,
+  Button,
+  Paper,
+  Input
+} from "@mui/material";
+
+function PlayerSelector() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default PlayerSelector;
